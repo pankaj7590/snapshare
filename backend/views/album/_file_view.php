e41bc2b400d95b4e62a921f3250d704ab4f3c99e
Pankaj Salokhe <pankajs7590@gmail.com>
2019-11-10 11:42:15 +0530

    lightgallery added on album view page

--- backend/views/album/_file_view.php
+++ backend/views/album/_file_view.php
@@ -8,17 +8,19 @@ if($model->isImage()){
 	$preview = Media::getDummyImage();
 }
 ?>
-<div class="file-container">
-	<div class="file-image-container">
-		<img src="<?= $preview?>" alt="<?= $model->alt?>" title="<?= $model->alt?>" class="img-responsive">
-	</div>
-	<span class="file-name" title="<?= Html::encode($model->alt)?>"><?= Html::encode($model->alt)?></span>
-	<span class="users-list-date">On <?= date('d M, Y', $model->created_at)?></span>
-	<span class="users-list-date">Size: <?= $model->fileSize?></span>
-	<div class="action-buttons">
-		<?php if($model->isImage()){?>
-			<?= Html::a('<i class="fa fa-image"></i>', ['album/set-cover', 'album' => $model->album->slug, 'image' => $model->slug], ['class' => ($model->albumCover?'btn btn-success':'btn btn-info'), 'data' => ['method' => 'post', 'confirm' => 'Do you really want to set it as cover image?']]);?>
-		<?php }?>
-		<?= Html::a('<i class="fa fa-trash"></i>', ['album/delete-file', 'album' => $model->album->slug, 'file' => $model->slug], ['class' => 'btn btn-danger', 'data' => ['method' => 'post', 'confirm' => 'Do you really want to delete it?']]);?>
+<div class="users-list-name">
+	<div class="file-container">
+		<div class="file-image-container">
+			<img src="<?= $preview?>" alt="<?= $model->alt?>" title="<?= $model->alt?>" class="img-responsive">
+		</div>
+		<span class="file-name" title="<?= Html::encode($model->alt)?>"><?= Html::encode($model->alt)?></span>
+		<span class="users-list-date">On <?= date('d M, Y', $model->created_at)?></span>
+		<span class="users-list-date">Size: <?= $model->fileSize?></span>
+		<div class="action-buttons">
+			<?php if($model->isImage()){?>
+				<?= Html::a('<i class="fa fa-image"></i>', ['album/set-cover', 'album' => $model->album->slug, 'image' => $model->slug], ['class' => ($model->albumCover?'btn btn-success':'btn btn-info'), 'data' => ['method' => 'post', 'confirm' => 'Do you really want to set it as cover image?']]);?>
+			<?php }?>
+			<?= Html::a('<i class="fa fa-trash"></i>', ['album/delete-file', 'album' => $model->album->slug, 'file' => $model->slug], ['class' => 'btn btn-danger', 'data' => ['method' => 'post', 'confirm' => 'Do you really want to delete it?']]);?>
+		</div>
 	</div>
 </div>
--- backend/views/album/view.php
+++ backend/views/album/view.php
@@ -5,6 +5,7 @@ use yii\widgets\DetailView;
 use common\models\Album;
 use yii\widgets\ListView;
 use common\models\Media;
+use yii\web\View;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Album */
@@ -71,10 +72,22 @@ $this->params['breadcrumbs'][] = $this->title;
 					<div class="container-fluid">
 						<div class="row">
 							<?= ListView::widget([
+								'id' => 'lightGallery',
+								'layout' => '{items}',
 								'dataProvider' => $dataProvider,
-								'itemOptions' => ['class' => 'item col-md-2'],
+								'itemOptions' => function($model, $key, $index, $widget){
+									if($model->isImage()){
+										$preview = Yii::$app->urlManager->baseUrl.'/uploads/'.$model->file_name;
+									}else{
+										$preview = Media::getDummyImage();
+									}
+									return [
+										'class' => 'col-md-2', 
+										'data-src' => $preview,
+									];
+								},
 								'itemView' => function ($model, $key, $index, $widget) {
-									return Html::a($this->render('_file_view', ['model' => $model]), null, ['class' => 'users-list-name']);
+									return $this->render('_file_view', ['model' => $model]);
 								},
 							]) ?>
 						</div>
@@ -83,6 +96,13 @@ $this->params['breadcrumbs'][] = $this->title;
 			</div>
 		</div>
 	</div>
+<?php
+$this->registerCssFile(Yii::$app->urlManager->baseUrl.'/js/lightgallery/css/lightgallery.min.css');
+$this->registerJsFile(Yii::$app->urlManager->baseUrl.'/js/lightgallery/js/lightgallery-all.min.js', ['depends' => \yii\web\JqueryAsset::className()]);
+$this->registerJs("
+	$('#lightGallery').lightGallery();
+", View::POS_READY);
+?>
 <?php
 $this->registerCss("
 .file-image-container{
